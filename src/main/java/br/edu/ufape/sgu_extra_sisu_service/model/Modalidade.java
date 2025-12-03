487977f052c39b350980c469a108c12015723d62
Thiago2704 <thiagoaraujo909090@gmail.com>
2025-12-03 17:29:09 -0300

    Adicionando models

---
+++ src/main/java/br/edu/ufape/sgu_extra_sisu_service/model/Modalidade.java
@@ -0,0 +1,22 @@
+package br.edu.ufape.sgu_extra_sisu_service.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Modalidade {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String nome;
+    String descricao;
+}
---
+++ src/main/java/br/edu/ufape/sgu_extra_sisu_service/model/ModalidadesEdital.java
@@ -0,0 +1,27 @@
+package br.edu.ufape.sgu_extra_sisu_service.model;
+
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class ModalidadesEdital {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    Long edital_id;
+
+    @OneToMany
+    private List<Modalidade> modalidades;
+}
